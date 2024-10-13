c9b3a4870f792fcf6a1be32ac39ca7c1a5aedc43
MAC <mac@MACs-MacBook-Pro.local>
2024-10-13 23:07:16 +0200

    Site is down update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+
 import './App.css';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Home from './components/pages/Home'
-import Contact from './components/pages/Contact'
-import Services from './components/pages/Services';
+
 
 
 
 function App() {
   return (
-    <Router>
-       <Navbar></Navbar>
-       
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/Home" element={<Home/>}/>
-          <Route path="/Contact" element={<Contact/>}/>
-          <Route path="/Services" element={<Services/>}/>
-
-          
-        </Routes>
+    <div className="App">
+    <header className="App-header">
+     
+      <p>Site is down</p>
       
-    </Router>
+    </header>
+  </div>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,11 +10,15 @@ import Footer from '../Footer/Footer.js'
 
 function Home(){
     return(
-        <>
-        <Hero></Hero>
-        <Card/>
-        <Footer/>
-        </>
+        <div className="App">
+        <header className="App-header">
+          <a href='http://twisk.co.za'>
+          <img src={logo} className="App-logo" alt="logo"/>
+          </a>
+          <p>Site is down</p>
+        </header>
+      </div>
+
     );
 }
 export default Home;
